a5a4d1afdd0c463c6bc64e75d5b0e7eb2fbf749c
Pete Cartwright <pete.cartwright@gmail.com>
2017-04-02 22:43:57 -0400

    use render_template(), add a first template

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@
 import sqlite3
 import datetime
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -31,6 +31,7 @@ def get_store_info_string(store):
     region = store['region']
     distance_from_zip = store['distance_from_zip']
     open_time = store['open_at']
+    zip_code = store['search_zip']
 
     info_string = u"""<br>
            Model Name: {model_name}<br>
@@ -38,9 +39,10 @@ def get_store_info_string(store):
               Address: {address}<br>
                  City: {city}<br>
                Region: {region}<br>
+                  Zip: {zip_code}<br>
              Opens At: {open_time}<br>
            Miles away: {distance_from_zip}<br>
-            """.format(model_name=model_name, store_name=store_name, address=address, city=city, region=region, open_time=open_time, distance_from_zip=distance_from_zip)
+            """.format(model_name=model_name, store_name=store_name, address=address, city=city, region=region, zip_code=zip_code, open_time=open_time, distance_from_zip=distance_from_zip)
     info_string = info_string + u'\n------------------------------------------------------------------'
 
     return info_string
@@ -48,35 +50,24 @@ def get_store_info_string(store):
 
 @app.route("/")
 def bestbuy():
-    database_path = '/home/pete/sites/switch_checker/stores.db'
+    # database_path = '/home/pete/sites/switch_checker/stores.db'
+    database_path = 'stores.db'
     today = datetime.datetime.today().strftime("%Y-%m-%d")
     select_string = "select * from stores where date_checked = '{today}'".format(today=today)
     conn = sqlite3.connect(database_path)
     c = conn.cursor()
     results = c.execute(select_string)
+    # put the results into an easier-to-work with dict
     store_list = [dict((results.description[i][0], value) for i, value in enumerate(row)) for row in results.fetchall()]
 
-    total_count = len(store_list)
     within_25_miles = [x for x in store_list if float(x['distance_from_zip']) <= 25]
     within_50_miles = [x for x in store_list if float(x['distance_from_zip']) <= 50]
     within_100_miles = [x for x in store_list if float(x['distance_from_zip']) <= 100]
 
-    html = """
-    <br>
-    <h3>Found {num_stores} Switches within 500 miles of you</h3>
-    <h3>Found {within_100_miles} Switches within 100 miles of you</h3>
-    <h3>Found {within_50_miles} Switches within 50 miles of you</h3>
-    <h3>Found {within_25_miles} Switches within 25 miles of you</h3>
-    <br><br>
-    """.format(num_stores=total_count, within_100_miles=len(within_100_miles), within_50_miles=len(within_50_miles), within_25_miles=len(within_25_miles))
-
-    if  within_100_miles:
-        html = html + "<br>----------------------------------------------------------<br>"
-        html = html + "The options within 25 miles are:<br>"
-        for store in within_100_miles:
-            html = html + get_store_info_string(store=store, zip_code='23223')
-
-    return html
+    return render_template('index.html',
+                           store_list=store_list,
+                           within_25_miles=within_25_miles
+                          )
 
 
 if __name__ == "__main__":
