624da1760fb242eac28dee000cd14307c1d70413
Pete Cartwright <pete.cartwright@gmail.com>
2017-04-03 13:22:01 -0400

    typo, add google api key to config

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 """
-    Config file for switch-checker
-
+    Config file for siwtch-checker
+    
 """
-
 
 # get your api key from https://developer.bestbuy.com
 BEST_BUY_API_KEY = ''
 # https://www.zipcodeapi.com
 ZIP_CODE_API_KEY = ''
+# for the embedded google maps, https://developers.google.com/maps/documentation/embed/guide#directions_mode
+GOOGLE_API_KEY = ''
