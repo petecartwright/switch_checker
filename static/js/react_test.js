1979853af19d768acf7b45ee1c6d296d757e4baf
Pete Cartwright <pete.cartwright@gmail.com>
2017-04-06 13:04:03 -0400

    add search bar, some text formatting

--- static/js/react_test.js
+++ static/js/react_test.js
@@ -1,7 +1,7 @@
 class BBStoreRow extends React.Component {
 
   render() {
-    var model_name = this.props.store.model_name.replace('Nintendo - Switch™ 32GB Console - ', '').replace('Joy-Con™', '');
+    var model_name = this.props.store.model_name.replace('Nintendo - Switch™ 32GB Console - ', '').replace('Joy-Con™', '').replace('Neon Red', 'Red').replace('Neon Blue', 'Blue');
     return React.createElement(
       'tr',
       null,
@@ -45,6 +45,9 @@ class BBStoreTable extends React.Component {
   render() {
     var rows = [];
     this.props.stores.forEach(store => {
+      if (store.region.indexOf(this.props.filterText) === -1) {
+        return;
+      }
       rows.push(React.createElement(BBStoreRow, { store: store, key: store.reactKey }));
     });
     return React.createElement(
@@ -92,16 +95,53 @@ class BBStoreTable extends React.Component {
   }
 }
 
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+  }
+
+  handleFilterTextInputChange(e) {
+    this.props.onFilterTextInput(e.target.value);
+  }
+
+  render() {
+    return React.createElement(
+      'form',
+      { className: 'search-bar' },
+      React.createElement('input', { type: 'text',
+        placeholder: 'Filter by State...',
+        value: this.props.filterText,
+        onChange: this.handleFilterTextInputChange
+      })
+    );
+  }
+}
+
 class FilterableBBStoreTable extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { filterText: '' };
+    this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
   }
+
+  handleFilterTextInput(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+
   render() {
     return React.createElement(
       'div',
       null,
+      React.createElement(SearchBar, {
+        filterText: this.state.filterText,
+        onFilterTextInput: this.handleFilterTextInput
+      }),
       React.createElement(BBStoreTable, {
-        stores: this.props.stores
+        stores: this.props.stores,
+        filterText: this.state.filterText
       })
     );
   }
@@ -134,11 +174,9 @@ function update_intro_line(stores) {
   document.getElementById('update-date').innerHTML = date_formatted;
 }
 
-var store_list;
 fetch('/bestbuy/stores').then(function (response) {
   return response.json();
 }).then(function (stores) {
-  store_list = stores;
   update_intro_line(stores);
   generateKeys(stores);
   ReactDOM.render(React.createElement(FilterableBBStoreTable, { stores: stores }), document.getElementById('container'));
