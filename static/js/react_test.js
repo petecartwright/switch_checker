257c719cd6486689e7005cc6b745df9223853f79
Pete Cartwright <pete.cartwright@gmail.com>
2017-04-06 22:23:53 -0400

    add link to google maps for every address

--- static/js/react_test.js
+++ static/js/react_test.js
@@ -1,7 +1,11 @@
 class BBStoreRow extends React.Component {
 
   render() {
-    var model_name = this.props.store.model_name.replace('Nintendo - Switch™ 32GB Console - ', '').replace('Joy-Con™', '').replace('Neon Red', 'Red').replace('Neon Blue', 'Blue');
+    var model_name = this.props.store.model_name.replace('Nintendo - Switch™ 32GB Console - ', '').replace('Joy-Con™', '');
+
+    var google_map_raw_url = `https://www.google.com/maps/place/${this.props.store.address},+${this.props.store.city},+${this.props.store.search_zip}`;
+    var google_map_encoded_url = encodeURI(google_map_raw_url);
+
     return React.createElement(
       'tr',
       null,
@@ -13,7 +17,11 @@ class BBStoreRow extends React.Component {
       React.createElement(
         'td',
         null,
-        this.props.store.address
+        React.createElement(
+          'a',
+          { href: google_map_encoded_url },
+          this.props.store.address
+        )
       ),
       React.createElement(
         'td',
@@ -45,11 +53,6 @@ class BBStoreTable extends React.Component {
   render() {
     var rows = [];
     this.props.stores.forEach(store => {
-      var region_lowercase = store.region.toLowerCase();
-      var filterText_lowercase = this.props.filterText.toLowerCase();
-      if (region_lowercase.indexOf(filterText_lowercase) === -1) {
-        return;
-      }
       rows.push(React.createElement(BBStoreRow, { store: store, key: store.reactKey }));
     });
     return React.createElement(
@@ -97,53 +100,16 @@ class BBStoreTable extends React.Component {
   }
 }
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
-  }
-
-  handleFilterTextInputChange(e) {
-    this.props.onFilterTextInput(e.target.value);
-  }
-
-  render() {
-    return React.createElement(
-      'form',
-      { className: 'search-bar' },
-      React.createElement('input', { type: 'text',
-        placeholder: 'Filter by State...',
-        value: this.props.filterText,
-        onChange: this.handleFilterTextInputChange
-      })
-    );
-  }
-}
-
 class FilterableBBStoreTable extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { filterText: '' };
-    this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
   }
-
-  handleFilterTextInput(filterText) {
-    this.setState({
-      filterText: filterText
-    });
-  }
-
   render() {
     return React.createElement(
       'div',
       null,
-      React.createElement(SearchBar, {
-        filterText: this.state.filterText,
-        onFilterTextInput: this.handleFilterTextInput
-      }),
       React.createElement(BBStoreTable, {
-        stores: this.props.stores,
-        filterText: this.state.filterText
+        stores: this.props.stores
       })
     );
   }
@@ -176,9 +142,11 @@ function update_intro_line(stores) {
   document.getElementById('update-date').innerHTML = date_formatted;
 }
 
+var store_list;
 fetch('/bestbuy/stores').then(function (response) {
   return response.json();
 }).then(function (stores) {
+  store_list = stores;
   update_intro_line(stores);
   generateKeys(stores);
   ReactDOM.render(React.createElement(FilterableBBStoreTable, { stores: stores }), document.getElementById('container'));
