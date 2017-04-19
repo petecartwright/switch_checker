f8047123a709fc3ac2ad11d43f028ef980554671
Pete Cartwright <pete.cartwright@gmail.com>
2017-04-19 10:24:58 -0400

    generalize the database paramater

--- switch_checker/switch_checker.py
+++ switch_checker/switch_checker.py
@@ -48,7 +48,7 @@ def create_database_if_missing(db_name):
                                 search_zip          text,
                                 distance_from_zip   text,
                                 phone_number        text
-                                )
+                                );
                              """
     c.execute(table_creation_string)
     
@@ -58,7 +58,7 @@ def create_database_if_missing(db_name):
     return True
 
 
-def add_one_store_to_database(store):
+def add_one_store_to_database(db_name, store):
     ''' Take a store dict and add it to the database with today's date
     '''
 
@@ -77,7 +77,7 @@ def add_one_store_to_database(store):
     for p in store['products']:
         model_name = p['name']
 
-        conn = sqlite3.connect(DATABASE_FILENAME)
+        conn = sqlite3.connect(db_name)
         c = conn.cursor()
 
         c.execute(u"""INSERT INTO stores (date_checked, model_name, store_name, address, city, region, open_at, search_zip, distance_from_zip, phone_number)
@@ -91,12 +91,13 @@ def add_one_store_to_database(store):
     return True
 
 
-def add_all_stores_to_database(stores):
-
-    create_database_if_missing(DATABASE_FILENAME)
+def add_all_stores_to_database(db_name, stores):
+    """ Take a database, list of stores, and add them if we haven't added some already today
+    """ 
+    create_database_if_missing(db_name)
     # make sure we haven't aready updated the database today
     sql = 'select max(date_checked) from stores;'
-    conn = sqlite3.connect(DATABASE_FILENAME)
+    conn = sqlite3.connect(db_name)
     c = conn.cursor()
     newest_date = c.execute(sql).fetchone()[0]
     conn.close()
@@ -105,7 +106,7 @@ def add_all_stores_to_database(stores):
 
     if newest_date != today:
         for s in stores:
-            add_one_store_to_database(store=s)
+            add_one_store_to_database(db_name=db_name, store=s)
     else:
         print 'already added to the database!'
 
@@ -240,7 +241,7 @@ def main():
 
         stores_sorted_by_distance = sorted(stores_with_product, key=lambda k: k['distance'])
 
-        add_all_stores_to_database(stores_sorted_by_distance)
+        add_all_stores_to_database(DATABASE_FILENAME, stores_sorted_by_distance)
 
         # if we have any stores returned, start getting the interesting ones
         if stores_sorted_by_distance:
