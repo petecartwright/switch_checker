18f2c3dc22d953f14a6aaf7183d12358fa200a50
Pete <pete@petecartwright.com>
2017-08-21 20:06:56 -0400

    fix replace typo - remove extra .

--- switch_checker/config.py
+++ switch_checker/config.py
@@ -4,7 +4,7 @@
 """
 
 # get your api key from https://developer.bestbuy.com
-BEST_BUY_API_KEY = ''
+BEST_BUY_API_KEY = 'dyeo9p8YlZCAHkmcLS832GEz'
 # https://www.zipcodeapi.com
 ZIP_CODE_API_KEY = ''
 # for the embedded google maps, https://developers.google.com/maps/documentation/embed/guide#directions_mode
